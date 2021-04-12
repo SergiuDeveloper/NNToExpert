2a48bcbcd77bcfe4a587127ca5ed4adbaa24952e
SergiuDev <sergiu.nistor@info.uaic.ro>
2021-04-12 20:01:51 +0300

    first commit

---
+++ rule_extraction.py
@@ -0,0 +1,71 @@
+import itertools
+from abc import ABC
+from typing import List
+import numpy as np
+import tensorflow as tf
+from sklearn.tree import DecisionTreeClassifier, _tree
+
+
+class ContinuousSubdomain():
+    def __init__(self, domain_low: float, domain_high: float, closed_low: bool, closed_high: bool, step: float):
+        self.values = np.arange(domain_low + (step if not closed_low else 0), domain_high - (step if not closed_high else 0), step)
+        
+class Domain(ABC):
+    def __init__(self, values: List[float]):
+        self.values: List[float] = sorted(values)
+        
+class DiscreteDomain(Domain):
+    def __init__(self, values: List[float]):
+        super().__init__(values)
+
+class ContinuousDomain(Domain):
+    def __init__(self, subdomains: List[ContinuousSubdomain]):
+        subdomains_values = [subdomain.values for subdomain in subdomains]
+        values = list(itertools.chain.fromiterable(subdomains_values))
+        super().__init__(values)
+
+class Variable():
+    def __init__(self, domain: Domain):
+        self.domain: Domain = domain
+        
+class NNToExpertHelper():
+    @staticmethod
+    def extract_rules(nn: tf.keras.Model, variables: List[Variable]) -> List[str]:
+        NNToExpertHelper.rules_count = 0
+        NNToExpertHelper.rules_text = []
+    
+        variable_names = ['V{}'.format(i) for i in range(len(variables))]
+        variables_domain_values = [variable.domain.values for variable in variables]
+        variables_domains_cartesian_product = list(itertools.product(*variables_domain_values))
+        predicted_values = nn.predict(variables_domains_cartesian_product)
+        predicted_values = [int(predicted_value[0] > 0.5) for predicted_value in predicted_values]
+        
+        clf = DecisionTreeClassifier()
+        clf.fit(variables_domains_cartesian_product, predicted_values)
+        NNToExpertHelper.__tree_to_rules(clf, variable_names)
+        
+        return NNToExpertHelper.rules_text
+        
+    @staticmethod
+    def __tree_to_rules(clf, feature_names):
+        feature_names = [feature_names[i] if i != _tree.TREE_UNDEFINED else 'undefined' for i in clf.tree_.feature]
+        NNToExpertHelper.__tree_to_rules_aux(clf, 0, 1, feature_names, '')
+            
+    @staticmethod
+    def __tree_to_rules_aux(clf, node, depth, feature_names, current_rules_text):
+        if clf.tree_.feature[node] == _tree.TREE_UNDEFINED:
+            leaf_value = np.argmax(clf.tree_.value[node])
+
+            clips_input_vars = ['?{}'.format(feature_name) for feature_name in feature_names if feature_name != 'undefined']
+            input_line = '(input_vars {})'.format(' '.join(map(str, sorted(clips_input_vars))))
+            rule_text = '(defrule r{}\n\t{}\n{}=>\n\t(assert (output_vars {}))\n)\n'.format(NNToExpertHelper.rules_count, input_line, current_rules_text, leaf_value)
+            NNToExpertHelper.rules_text.append(rule_text)
+                
+            NNToExpertHelper.rules_count += 1
+            return
+            
+        left_rules_text = '{}\t(test (<= ?{} {}))\n'.format(current_rules_text, feature_names[node], clf.tree_.threshold[node])
+        NNToExpertHelper.__tree_to_rules_aux(clf, clf.tree_.children_left[node], depth + 1, feature_names, left_rules_text)
+            
+        right_rules_text = '{}\t(test (> ?{} {}))\n'.format(current_rules_text, feature_names[node], clf.tree_.threshold[node])
+        NNToExpertHelper.__tree_to_rules_aux(clf, clf.tree_.children_right[node], depth + 1, feature_names, right_rules_text)
---
+++ test.clp
@@ -0,0 +1,22 @@
+(defrule r0
+	(input_vars ?V0 ?V1)
+	(test (<= ?V0 0.5))
+=>
+	(assert (output_vars 0))
+)
+
+(defrule r1
+	(input_vars ?V0 ?V1)
+	(test (> ?V0 0.5))
+	(test (<= ?V1 0.5))
+=>
+	(assert (output_vars 0))
+)
+
+(defrule r2
+	(input_vars ?V0 ?V1)
+	(test (> ?V0 0.5))
+	(test (> ?V1 0.5))
+=>
+	(assert (output_vars 1))
+)
